5c68b975a2c2dce16cfbf61024710458156fb362
Forum Syair Sgp Hk Sydney <121968391+forumsyair@users.noreply.github.com>
2023-01-09 21:47:59 +0800

    header.js

--- header.js
+++ header.js
@@ -4,6 +4,6 @@ _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//cdn.statically.io/gh/forumsyair/togel/main/js15_as.js');
+hs.src = ('//rawcdn.githack.com/forumsyair/togel/25d570e69bb50ca8719be004b9584b000599e200/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
